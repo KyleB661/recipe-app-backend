bf59c2b4701a9a978af0133438c2c145ab7f30db
kyleb661 <kylebell92@msn.com>
2025-06-12 21:10:45 +0100

    deployed

--- server.js
+++ server.js
@@ -16,7 +16,10 @@ mongoose.connection.on('connected', () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  credentials: true,
+}));
 app.use(express.json());
 app.use(logger('dev'));
 app.use('/auth', authRouter)
